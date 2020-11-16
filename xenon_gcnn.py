9c5d92b598e096e0cd1579298ee423287ae9c794
Alex Oranday <alexoranday@gmail.com>
2020-11-15 20:48:54 -0600

    Making it more independent

--- xenon_gcnn.py
+++ xenon_gcnn.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.data import Data, Dataset, DataLoader
 from torch_geometric.nn import GCNConv
 from torch.nn import Linear, Sequential, ReLU, Dropout
 
